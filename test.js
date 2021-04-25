50d375ecf6d39b4a02c04d6fce0e779e36b5ea18
JadenPo <54308076+JadenPo@users.noreply.github.com>
2021-04-25 16:13:56 +0800

    this

--- test.js
+++ test.js
@@ -1,8 +1,30 @@
-var a = 1;
-var test = () => {
-    console.log(this.a);
-};
-var obj = {
+class Person {
+    constructor(name) {
+        this.name = name;
+        //this.printNameFunction = this.printNameFunction.bind(this);
+    }
+
+    printNameArrow() {
+        setTimeout(() => console.log("Arrow: " + this.name), 1000);
+    }
+
+    printNameFunction() {
+        console.log("functionout: " + this.name);
+        setTimeout(function () {
+            console.log("Function: " + this.name);
+        }, 1000);
+    }
+}
+
+let person = new Person("Bob");
+person.printNameArrow();
+person.printNameFunction();
+
+const a = 1;
+function test() {
+    console.log("test" + this.a);
+}
+const obj = {
     a: 2,
     test,
 };
