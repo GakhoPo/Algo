3d20f9f1aec854659e4e21f4d733ee9d3d337367
JadenPo <54308076+JadenPo@users.noreply.github.com>
2021-05-08 21:27:22 +0800

    push

--- 4_new手写.js
+++ 4_new手写.js
@@ -3,5 +3,5 @@ function _new() {
     let Con = [].shift.call(arguments);
     obj.__proto__ = Con.prototype;
     let res = Con.apply(obj, arguments);
-    return typeof result === "object" ? res : obj;
+    return typeof res === "object" ? res : obj;
 }
