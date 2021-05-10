cf7fadcdd7dddf74d72654d26104c4c051f16f49
JadenPo <54308076+JadenPo@users.noreply.github.com>
2021-05-10 16:53:39 +0800

    parse

--- 手写parse.js
+++ 手写parse.js
@@ -1,37 +1,40 @@
 const str1 = "[ 1, 2, [[3], [4, 5 ,6,[]]]]";
-const str = "[ 11, [23], 3 ]";
+const str = "[ 1, [23], 3 ]";
 
 function newParse(str) {
-    var i = 0;
-    function parseJudge() {
-        while (str[i] === " ") i++;
-        if (str[i] === "[") {
-            return parseArray();
-        } else {
-            return parseValue();
+    try {
+        var i = 0;
+        str = str.replace(/\s+/g, "");
+        function judge() {
+            while (str[i] === " ") i++;
+            if (str[i] === "[") return parseArr();
+            else if (str[i] >= 0 && str[i] <= 9) return parseNum();
+            else throw new Error("invalid array");
         }
-    }
-
-    function parseArray() {
-        i++;
-        var result = [];
-        while (str[i] !== "]") {
-            result.push(parseJudge());
-            if (str[i] === ",") i++;
-        }
-        i++;
-        return result;
-    }
-
-    function parseValue() {
-        let num = "";
-        while (str[i] >= 0 && str[i] <= 9) {
-            num += str[i++];
+        function parseArr() {
+            var res = [];
+            i++;
+            //if (str[i] === ",")
+            while (str[i] !== "]") {
+                res.push(judge());
+                if (str[i] === ",") i++;
+            }
+            i++;
+            return res;
         }
 
-        return parseInt(num);
+        function parseNum() {
+            let num = "";
+            while (str[i] >= 0 && str[i] <= 9) {
+                num += str[i];
+                i++;
+            }
+            return parseInt(num);
+        }
+        return judge();
+    } catch (e) {
+        console.log(e);
     }
-    return parseJudge();
 }
 console.log(newParse(str1));
-console.log(newParse(str1));
+console.log(newParse(str));
