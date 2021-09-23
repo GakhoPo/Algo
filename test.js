8a3d0a5537aca0aa238163a45b6d009ecf750fac
JadenPo <54308076+JadenPo@users.noreply.github.com>
2021-09-23 22:45:13 +0800

    busemmiter

---
+++ test.js
@@ -0,0 +1,29 @@
+class EventBus {
+    constructor() {
+        this.handlers = {};
+    }
+
+    publish(type, value) {
+        let fns = this.handlers[type];
+        if (Array.isArray(fns)) {
+            fns.forEach((fn) => fn(value));
+        }
+    }
+
+    subscribe(type, func) {
+        let fns = this.handlers[type] || [];
+        if (fns.indexOf(func) === -1) fns.push(func);
+    }
+
+    remove(type, func) {
+        if (!type) this.handlers = {};
+        else {
+            let fns = this.handlers[type] || [];
+            if (!func) fns = [];
+            else if (Array.isArray(fns)) {
+                let index = fns.indexOf(func);
+                if (index !== -1) fns.splice(index, 1);
+            }
+        }
+    }
+}
